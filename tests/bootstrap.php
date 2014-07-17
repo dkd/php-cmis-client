f38c77a69d5343edb070799caf4550da07c86a48
Sascha Egerer <sascha.egerer@dkd.de>
2014-07-17 11:14:52 +0200

    Rename composer package, change some package basics
    Summary:
    Renamed the composer package from
    `dkd/php-cmis-client` to `dkd/php-cmis` and
    changed the folder structure and the autoloader
    configuration.
    
    Reviewers: #forgetit-dev, dkd-doan
    
    Reviewed By: #forgetit-dev, dkd-doan
    
    Differential Revision: https://phabricator.dkd.de/D72

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,11 @@
  * bootstrap.php
  */
 
-// Composer
+// Register composer autoloader
 if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
-	throw new \RuntimeException('Could not find vendor/autoload.php, make sure you ran composer.');
+    throw new \RuntimeException(
+        'Could not find vendor/autoload.php, make sure you ran composer.'
+    );
 }
 
 require_once __DIR__.'/../vendor/autoload.php';
