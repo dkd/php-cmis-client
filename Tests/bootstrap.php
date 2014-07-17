774d075f392fd9b755246a03f40524cc7d72c576
Sascha Egerer <sascha.egerer@dkd.de>
2014-07-17 10:04:28 +0200

    Add PHP Unit and configured testing framework
    Summary:
    Added PHP Unit composer
    package and added a bootstrapper
    file to get the Tests running.
    
    Test Plan:
    * composer install --dev
    * arc unit Tests/MyExampleTest.php (this file has to be created)
    
    Reviewers: #forgetit-dev, dkd-doan
    
    Reviewed By: #forgetit-dev, dkd-doan
    
    Differential Revision: https://phabricator.dkd.de/D70

---
+++ Tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * bootstrap.php
+ */
+
+// Composer
+if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
+	throw new \RuntimeException('Could not find vendor/autoload.php, make sure you ran composer.');
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
