a598ee9c77820be16a828eb9acf7feb75411c923
Sascha Egerer <sascha.egerer@dkd.de>
2014-10-07 10:07:15 +0200

    Add Mutable Property Interfaces
    Summary:
    Add Interfaces for Mutable
    Properties
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D286

---
+++ src/Data/MutablePropertyDataInterface.php
@@ -0,0 +1,65 @@
+<?php
+namespace Dkd\PhpCmis\Data;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Mutable PropertyData.
+ */
+interface MutablePropertyDataInterface extends PropertyDataInterface
+{
+    /**
+     * Sets the property ID.
+     *
+     * @param string $id the property ID, should not be {@code null}
+     */
+    public function setId($id);
+
+    /**
+     * Set the display name.
+     *
+     * @param string $displayName the display name
+     */
+    public function setDisplayName($displayName);
+
+    /**
+     * Set the local name.
+     *
+     * @param string $localName the local name
+     */
+    public function setLocalName($localName);
+
+    /**
+     * Set the query name.
+     *
+     * @param string $queryName the query name
+     */
+    public function setQueryName($queryName);
+
+    /**
+     * Sets the property value.
+     *
+     * If this property is a single value property, this list must either be
+     * empty or {@code null} (= unset) or must only contain one entry.
+     *
+     * @param array $values the property value or {@code null} to unset the property
+     */
+    public function setValues($values);
+
+    /**
+     * Sets a property value.
+     *
+     * If this property is a multi value property, this value becomes the only
+     * value in the list of values.
+     *
+     * @param mixed $value the property value or {@code null} to unset the property
+     */
+    public function setValue($value);
+}
