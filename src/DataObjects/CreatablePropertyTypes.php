743db6fd72336bed75235bdb4461149a23839a29
Sascha Egerer <sascha.egerer@dkd.de>
2014-10-14 21:54:32 +0200

    Add Creatable Property Types Data Object
    Summary:
    Adds a creatable propert types
    data object
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D328

---
+++ src/DataObjects/CreatablePropertyTypes.php
@@ -0,0 +1,47 @@
+<?php
+namespace Dkd\PhpCmis\DataObjects;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\Data\CreatablePropertyTypesInterface;
+use Dkd\PhpCmis\Enum\PropertyType;
+
+/**
+ * Repository info data implementation.
+ */
+class CreatablePropertyTypes extends AbstractExtensionData implements CreatablePropertyTypesInterface
+{
+    /**
+     * @var PropertyType[]
+     */
+    protected $propertyTypeSet = array();
+
+    /**
+     * Returns the set of property data types that can used to create or update a type definition.
+     *
+     * @return PropertyType[] the available set of property data types
+     */
+    public function canCreate()
+    {
+        return $this->propertyTypeSet;
+    }
+
+    /**
+     * @param PropertyType[] $propertyTypeSet
+     */
+    public function setCanCreate(array $propertyTypeSet)
+    {
+        foreach ($propertyTypeSet as $propertyType) {
+            $this->checkObjectType('\\Dkd\\PhpCmis\\Enum\\PropertyType', $propertyType);
+        }
+
+        $this->propertyTypeSet = $propertyTypeSet;
+    }
+}
---
+++ tests/Unit/DataObjects/CreatablePropertyTypesTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace Dkd\PhpCmis\Test\Unit\DataObjects;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\DataObjects\CreatablePropertyTypes;
+use Dkd\PhpCmis\Enum\PropertyType;
+
+class CreatablePropertyTypesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CreatablePropertyTypes
+     */
+    protected $creatablePropertyTypes;
+
+    public function setUp()
+    {
+        $this->creatablePropertyTypes = new CreatablePropertyTypes();
+    }
+
+    public function testSetCanCreateSetsProperty()
+    {
+        $types = array(PropertyType::cast(PropertyType::DATETIME));
+
+        $this->creatablePropertyTypes->setCanCreate($types);
+        $this->assertAttributeSame($types, 'propertyTypeSet', $this->creatablePropertyTypes);
+    }
+
+    /**
+     * @dataProvider invalidPropertyTypesDataProvider
+     * @param $propertyTypes
+     * @param $expectedExceptionText
+     */
+    public function testSetCanCreateThrowsExceptionIfInvalidAttributeGiven(
+        $propertyTypes,
+        $expectedExceptionText
+    ) {
+        $this->setExpectedException('\\InvalidArgumentException', $expectedExceptionText);
+        $this->creatablePropertyTypes->setCanCreate(array($propertyTypes));
+    }
+
+    public function invalidPropertyTypesDataProvider()
+    {
+        return array(
+            array(
+                'string',
+                'Argument of type "string" given but argument of type '
+                . '"\\Dkd\\PhpCmis\\Enum\\PropertyType" was expected.'
+            ),
+            array(
+                0,
+                'Argument of type "integer" given but argument of type '
+                . '"\\Dkd\\PhpCmis\\Enum\\PropertyType" was expected.'
+            ),
+            array(
+                array(),
+                'Argument of type "array" given but argument of type '
+                . '"\\Dkd\\PhpCmis\\Enum\\PropertyType" was expected.'
+            ),
+            array(
+                new \stdClass(),
+                'Argument of type "stdClass" given but argument of type '
+                . '"\\Dkd\\PhpCmis\\Enum\\PropertyType" was expected.'
+            )
+        );
+    }
+
+    /**
+     * @depends testSetCanCreateSetsProperty
+     */
+    public function testCanCreateReturnsProperty()
+    {
+        $types = array(PropertyType::cast(PropertyType::DATETIME));
+        $this->creatablePropertyTypes->setCanCreate($types);
+        $this->assertSame($types, $this->creatablePropertyTypes->canCreate());
+    }
+}
