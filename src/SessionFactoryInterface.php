8ce6386267948d4cb5a5642c22517075e5937940
Sascha Egerer <sascha.egerer@dkd.de>
2014-10-14 12:44:58 +0200

    Import missing class in SessionFactoryInterface
    Summary:
    The class `Repository` is used in PHPDoc
    but not imported in the class.
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D307

--- src/SessionFactoryInterface.php
+++ src/SessionFactoryInterface.php
@@ -9,6 +9,7 @@ namespace Dkd\PhpCmis;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+use Dkd\PhpCmis\DataObjects\Repository;
 
 /**
  * Entry point into the php cmis lib Client API.
