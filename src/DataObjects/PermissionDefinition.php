1e197ba083ee0d85482b15129952f8405be86ddd
Sascha Egerer <sascha.egerer@dkd.de>
2014-10-15 15:29:30 +0200

    Add Permission Definition Data Object
    Summary:
    Add a Permission Definition
    Data Object implementation.
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D333

---
+++ src/DataObjects/PermissionDefinition.php
@@ -0,0 +1,61 @@
+<?php
+namespace Dkd\PhpCmis\DataObjects;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\Definitions\PermissionDefinitionInterface;
+
+/**
+ * Permission definition.
+ */
+class PermissionDefinition extends AbstractExtensionData implements PermissionDefinitionInterface
+{
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = (string) $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = (string) $id;
+    }
+}
---
+++ tests/Unit/DataObjects/PermissionDefinitionTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace Dkd\PhpCmis\Test\Unit\DataObjects;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\DataObjects\PermissionDefinition;
+use Dkd\PhpCmis\Test\Unit\DataProviderCollectionTrait;
+use PHPUnit_Framework_TestCase;
+
+class PermissionDefinitionTest extends PHPUnit_Framework_TestCase
+{
+    use DataProviderCollectionTrait;
+
+    /**
+     * @var PermissionDefinition
+     */
+    protected $permissionDefinition;
+
+    public function setUp()
+    {
+        $this->permissionDefinition = new PermissionDefinition();
+    }
+
+    /**
+     * @dataProvider stringCastDataProvider
+     */
+    public function testSetDescriptionSetsProperty($expected, $value)
+    {
+        $this->permissionDefinition->setDescription($value);
+        $this->assertAttributeSame($expected, 'description', $this->permissionDefinition);
+    }
+
+    /**
+     * @depends testSetDescriptionSetsProperty
+     */
+    public function testGetDescriptionReturnsPropertyValue()
+    {
+        $this->permissionDefinition->setDescription('foo');
+        $this->assertSame('foo', $this->permissionDefinition->getDescription());
+    }
+
+    /**
+     * @dataProvider stringCastDataProvider
+     */
+    public function testSetIdSetsProperty($expected, $value)
+    {
+        $this->permissionDefinition->setId($value);
+        $this->assertAttributeSame($expected, 'id', $this->permissionDefinition);
+    }
+
+    /**
+     * @depends testSetIdSetsProperty
+     */
+    public function testGetIdReturnsPropertyValue()
+    {
+        $this->permissionDefinition->setId('foo');
+        $this->assertSame('foo', $this->permissionDefinition->getId());
+    }
+}
