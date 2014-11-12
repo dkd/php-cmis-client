a8029e5a44e20df53289ca95957fda11b33d5d52
Sascha Egerer <sascha.egerer@dkd.de>
2014-11-12 10:48:15 +0100

    Add Repository Url Cache for Browser binding
    Summary:
    Add a repository url cache for the browser binding which is used to
    build some basic urls based some repository infos that have been cached.
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D370

---
+++ src/Bindings/Browser/RepositoryUrlCache.php
@@ -0,0 +1,165 @@
+<?php
+namespace Dkd\PhpCmis\Bindings\Browser;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\Constants;
+use Dkd\PhpCmis\Exception\CmisInvalidArgumentException;
+use League\Url\Url;
+
+/**
+ * URL cache for repository and root URLs.
+ */
+class RepositoryUrlCache
+{
+    /**
+     * @var array
+     */
+    protected $repositoryUrls;
+
+    /**
+     * @var array
+     */
+    protected $rootUrls;
+
+    /**
+     * Adds the URLs of a repository to the cache.
+     *
+     * @param string $repositoryId
+     * @param string $repositoryUrl
+     * @param string $rootUrl
+     * @return void
+     */
+    public function addRepository($repositoryId, $repositoryUrl, $rootUrl)
+    {
+        if (empty($repositoryId) || empty($repositoryUrl) || empty($rootUrl)) {
+            throw new CmisInvalidArgumentException(
+                'Repository Id or Repository URL or Root URL is not set!',
+                1408536098
+            );
+        }
+
+        $this->repositoryUrls[$repositoryId] = $repositoryUrl;
+        $this->rootUrls[$repositoryId] = $rootUrl;
+    }
+
+    /**
+     * Removes the URLs of a repository from the cache.
+     *
+     * @param string $repositoryId
+     */
+    public function removeRepository($repositoryId)
+    {
+        unset($this->repositoryUrls[$repositoryId]);
+        unset($this->rootUrls[$repositoryId]);
+    }
+
+    /**
+     * @param string $repositoryId
+     * @return string|null
+     */
+    public function getRepositoryBaseUrl($repositoryId)
+    {
+        return isset($this->repositoryUrls[$repositoryId]) ? $this->repositoryUrls[$repositoryId] : null;
+    }
+
+    /**
+     * Returns the repository URL of a repository.
+     *
+     * @param string $repositoryId
+     * @param string $selector add optional cmis selector parameter
+     * @return Url|null
+     */
+    public function getRepositoryUrl($repositoryId, $selector = null)
+    {
+        if ($this->getRepositoryBaseUrl($repositoryId) === null) {
+            return null;
+        } else {
+            $repositoryUrl = $this->buildUrl($this->getRepositoryBaseUrl($repositoryId));
+        }
+
+        if ($selector !== null && $selector !== '') {
+            $repositoryUrl->getQuery()->modify(array(Constants::PARAM_SELECTOR => $selector));
+        }
+
+        return $repositoryUrl;
+    }
+
+    /**
+     * Returns the root URL of a repository.
+     *
+     * @param string $repositoryId
+     * @return string
+     */
+    public function getRootUrl($repositoryId)
+    {
+        return isset($this->rootUrls[$repositoryId]) ? $this->rootUrls[$repositoryId] : null;
+    }
+
+    /**
+     * Get URL for an object request
+     *
+     * @param string $repositoryId
+     * @param string $objectId
+     * @param string $selector
+     * @return Url|null
+     */
+    public function getObjectUrl($repositoryId, $objectId, $selector = null)
+    {
+        if ($this->getRootUrl($repositoryId) === null) {
+            return null;
+        }
+
+        $url = $this->buildUrl($this->getRootUrl($repositoryId));
+        $urlQuery = $url->getQuery();
+        $urlQuery->modify(array(Constants::PARAM_OBJECT_ID => (string) $objectId));
+
+        if (!empty($selector)) {
+            $urlQuery->modify(array(Constants::PARAM_SELECTOR => (string) $selector));
+        }
+
+        return $url;
+    }
+
+    /**
+     * Get Repository URL with given path
+     *
+     * @param string $repositoryId
+     * @param string $path
+     * @param string $selector
+     * @return Url
+     */
+    public function getPathUrl($repositoryId, $path, $selector = null)
+    {
+        if ($this->getRootUrl($repositoryId) === null) {
+            return null;
+        }
+
+        $url = $this->buildUrl($this->getRootUrl($repositoryId));
+        $url->getPath()->append($path);
+
+        if (!empty($selector)) {
+            $url->getQuery()->modify(array(Constants::PARAM_SELECTOR => $selector));
+        }
+
+        return $url;
+    }
+
+    /**
+     * Build an instance of \League\Url\Url for the given url
+     *
+     * @param string $url
+     * @return Url
+     */
+    public function buildUrl($url)
+    {
+        return Url::createFromUrl($url);
+    }
+}
---
+++ tests/Unit/Bindings/Browser/RepositoryUrlCacheTest.php
@@ -0,0 +1,300 @@
+<?php
+namespace Dkd\PhpCmis\Test\Unit\Bindings\Browser;
+
+/**
+ * This file is part of php-cmis-lib.
+ *
+ * (c) Sascha Egerer <sascha.egerer@dkd.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Dkd\PhpCmis\Bindings\Browser\RepositoryUrlCache;
+use Dkd\PhpCmis\Constants;
+use League\Url\Url;
+
+class RepositoryUrlCacheTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RepositoryUrlCache
+     */
+    protected $repositoryUrlCache;
+
+    /**
+     * @var array
+     */
+    protected $dummyData = array(
+        array(
+            'id' => 'repositoryId',
+            'repositoryUrl' => 'http://foo.bar.baz/repositoryUrl',
+            'rootUrl' => 'http://foo.bar.baz/rootUrl'
+        ),
+        array(
+            'id' => 'repositoryId2',
+            'repositoryUrl' => 'http://foo.bar.baz/repositoryUrl2',
+            'rootUrl' => 'http://foo.bar.baz/rootUrl2'
+        ),
+    );
+
+    public function setUp()
+    {
+        $this->repositoryUrlCache = new RepositoryUrlCache();
+    }
+
+    /**
+     * @dataProvider addRepositoryParameterDataProvider
+     * @param $repositoryId
+     * @param $repositoryUrl
+     * @param $rootUrl
+     */
+    public function testAddRepositoryThrowsExceptionIfEmptyParameterGiven($repositoryId, $repositoryUrl, $rootUrl)
+    {
+        $this->setExpectedException('\\Dkd\\PhpCmis\\Exception\\CmisInvalidArgumentException');
+        $this->repositoryUrlCache->addRepository($repositoryId, $repositoryUrl, $rootUrl);
+    }
+
+    public function addRepositoryParameterDataProvider()
+    {
+        return array(
+            array(
+                null,
+                'foo',
+                'bar'
+            ),
+            array(
+                'foo',
+                null,
+                'bar'
+            ),
+            array(
+                'foo',
+                'bar',
+                null
+            ),
+        );
+    }
+
+    public function testAddRepositoryAddsGivenUrlsToArrayCache()
+    {
+        $this->repositoryUrlCache->addRepository(
+            $this->dummyData[0]['id'],
+            $this->dummyData[0]['repositoryUrl'],
+            $this->dummyData[0]['rootUrl']
+        );
+
+        $this->assertAttributeSame(
+            array($this->dummyData[0]['id'] => $this->dummyData[0]['repositoryUrl']),
+            'repositoryUrls',
+            $this->repositoryUrlCache
+        );
+        $this->assertAttributeSame(
+            array($this->dummyData[0]['id'] => $this->dummyData[0]['rootUrl']),
+            'rootUrls',
+            $this->repositoryUrlCache
+        );
+
+        $this->repositoryUrlCache->addRepository(
+            $this->dummyData[1]['id'],
+            $this->dummyData[1]['repositoryUrl'],
+            $this->dummyData[1]['rootUrl']
+        );
+
+        $this->assertAttributeSame(
+            array(
+                $this->dummyData[0]['id'] => $this->dummyData[0]['repositoryUrl'],
+                $this->dummyData[1]['id'] => $this->dummyData[1]['repositoryUrl']
+            ),
+            'repositoryUrls',
+            $this->repositoryUrlCache
+        );
+        $this->assertAttributeSame(
+            array(
+                $this->dummyData[0]['id'] => $this->dummyData[0]['rootUrl'],
+                $this->dummyData[1]['id'] => $this->dummyData[1]['rootUrl']
+            ),
+            'rootUrls',
+            $this->repositoryUrlCache
+        );
+
+        return $this->repositoryUrlCache;
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testRemoveRepositoryRemovesRepositoryFromCache($repositoryUrlCache)
+    {
+        $repositoryUrlCache = clone $repositoryUrlCache;
+        $repositoryUrlCache->removeRepository($this->dummyData[1]['id']);
+
+        $this->assertAttributeSame(
+            array($this->dummyData[0]['id'] => $this->dummyData[0]['repositoryUrl']),
+            'repositoryUrls',
+            $repositoryUrlCache
+        );
+        $this->assertAttributeSame(
+            array($this->dummyData[0]['id'] => $this->dummyData[0]['rootUrl']),
+            'rootUrls',
+            $repositoryUrlCache
+        );
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRepositoryBaseUrlReturnsUrlForGivenRepositoryId($repositoryUrlCache)
+    {
+        $this->assertSame(
+            $this->dummyData[0]['repositoryUrl'],
+            $repositoryUrlCache->getRepositoryBaseUrl($this->dummyData[0]['id'])
+        );
+        $this->assertSame(
+            $this->dummyData[1]['repositoryUrl'],
+            $repositoryUrlCache->getRepositoryBaseUrl($this->dummyData[1]['id'])
+        );
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRepositoryUrlReturnsNullIfNoBaseUrlForIdIsFoundInCache($repositoryUrlCache)
+    {
+        $this->assertNull($repositoryUrlCache->getRepositoryUrl('invalid-id'));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRepositoryUrlReturnsInstanceOfUrlBasedOnBaseUrlString($repositoryUrlCache)
+    {
+        $expectedUrl = Url::createFromUrl($this->dummyData[0]['repositoryUrl']);
+        $this->assertEquals($expectedUrl, $repositoryUrlCache->getRepositoryUrl($this->dummyData[0]['id']));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRepositoryUrlWithSelectorReturnsInstanceOfUrlBasedOnBaseUrlStringWithSelector(
+        $repositoryUrlCache
+    ) {
+        $selector = 'fooSelector';
+        $expectedUrl = Url::createFromUrl(
+            $this->dummyData[0]['repositoryUrl'] . '?' . Constants::PARAM_SELECTOR . '=' . $selector
+        );
+        $this->assertEquals($expectedUrl, $repositoryUrlCache->getRepositoryUrl($this->dummyData[0]['id'], $selector));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRootUrlReturnsNullIfNoUrlForIdIsFoundInCache($repositoryUrlCache)
+    {
+        $this->assertNull($repositoryUrlCache->getRootUrl('invalid-id'));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetRootUrlReturnsInstanceOfUrlBasedOnBaseUrlString($repositoryUrlCache)
+    {
+        $expectedUrl = Url::createFromUrl($this->dummyData[0]['rootUrl']);
+        $this->assertEquals($expectedUrl, $repositoryUrlCache->getRootUrl($this->dummyData[0]['id']));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetObjectUrlReturnsNullIfNoRootUrlForIdIsFoundInCache($repositoryUrlCache)
+    {
+        $this->assertNull($repositoryUrlCache->getObjectUrl('invalid-id', 'foo-id'));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetObjectUrlReturnsInstanceOfUrlBasedOnBaseUrlStringWithObjectIdSelector($repositoryUrlCache)
+    {
+        $objectId = 'object-id';
+        $expectedUrl = Url::createFromUrl(
+            $this->dummyData[0]['rootUrl'] . '?' . Constants::PARAM_OBJECT_ID . '=' . $objectId
+        );
+        $this->assertEquals($expectedUrl, $repositoryUrlCache->getObjectUrl($this->dummyData[0]['id'], $objectId));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetObjectUrlWithSelectorReturnsInstanceOfUrlBasedOnBaseUrlStringWithObjectIdSelectorAndSelector(
+        $repositoryUrlCache
+    ) {
+        $selector = 'fooSelector';
+        $objectId = 'object-id';
+        $expectedUrl = Url::createFromUrl(
+            $this->dummyData[0]['rootUrl'] . '?'
+            . Constants::PARAM_OBJECT_ID . '=' . $objectId . '&'
+            . Constants::PARAM_SELECTOR . '=' . $selector
+        );
+        $this->assertEquals(
+            $expectedUrl,
+            $repositoryUrlCache->getObjectUrl($this->dummyData[0]['id'], $objectId, $selector)
+        );
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetPathUrlReturnsNullIfNoRootUrlForIdIsFoundInCache($repositoryUrlCache)
+    {
+        $this->assertNull($repositoryUrlCache->getPathUrl('invalid-id', 'foo-path'));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetPathUrlReturnsInstanceOfUrlBasedOnBaseUrlStringWithObjectIdSelector($repositoryUrlCache)
+    {
+        $path = '/foo/bar/baz';
+        $expectedUrl = Url::createFromUrl(
+            $this->dummyData[0]['rootUrl'] . $path
+        );
+        $this->assertEquals($expectedUrl, $repositoryUrlCache->getPathUrl($this->dummyData[0]['id'], $path));
+    }
+
+    /**
+     * @depends testAddRepositoryAddsGivenUrlsToArrayCache
+     * @param $repositoryUrlCache RepositoryUrlCache
+     */
+    public function testGetPathUrlWithSelectorReturnsInstanceOfUrlBasedOnBaseUrlStringWithObjectIdSelectorAndSelector(
+        $repositoryUrlCache
+    ) {
+        $selector = 'fooSelector';
+        $path = '/foo/bar/baz';
+        $expectedUrl = Url::createFromUrl(
+            $this->dummyData[0]['rootUrl'] . $path . '?' . Constants::PARAM_SELECTOR . '=' . $selector
+        );
+        $this->assertEquals(
+            $expectedUrl,
+            $repositoryUrlCache->getPathUrl($this->dummyData[0]['id'], $path, $selector)
+        );
+    }
+
+    public function testBuildUrlReturnsUrlInstanceBasedOnGivenUrlString()
+    {
+        $urlString = 'http://foo.bar.baz';
+        $url = Url::createFromUrl($urlString);
+        $this->assertEquals($url, $this->repositoryUrlCache->buildUrl($urlString));
+    }
+}
