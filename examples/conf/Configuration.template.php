7db8eaa3e3105d19975e1b5be7d9b97cf03ffb6c
ksjogo <jogo@kronberger-spiele.de>
2016-02-15 10:19:26 +0100

    add default timezone in examples configuration like bootstrap.php for testing

--- examples/conf/Configuration.template.php
+++ examples/conf/Configuration.template.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Europe/Berlin');       // Set the default timezone
 define('CMIS_BROWSER_URL', 'http://my.alfresco.tld:8080/alfresco/api/-default-/public/cmis/versions/1.1/browser');
 define('CMIS_BROWSER_USER', 'admin');
 define('CMIS_BROWSER_PASSWORD', 'AlfrescoAdmin');
