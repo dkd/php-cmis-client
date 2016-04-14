0c2b984b7dbde91b32128cd79b85491c734ddd75
della-90 <della90@msn.com>
2016-04-14 12:12:28 +0200

    Implemented multi-filing method

--- src/Bindings/Browser/MultiFilingService.php
+++ src/Bindings/Browser/MultiFilingService.php
@@ -10,22 +10,22 @@ namespace Dkd\PhpCmis\Bindings\Browser;
  * file that was distributed with this source code.
  */
 
+use Dkd\PhpCmis\Constants;
 use Dkd\PhpCmis\Data\ExtensionDataInterface;
 use Dkd\PhpCmis\MultiFilingServiceInterface;
 
 /**
  * MultiFiling Service Browser Binding client.
  */
-class MultiFilingService extends AbstractBrowserBindingService implements MultiFilingServiceInterface
-{
+class MultiFilingService extends AbstractBrowserBindingService implements MultiFilingServiceInterface {
     /**
      * Adds an existing fileable non-folder object to a folder.
      *
-     * @param string $repositoryId The identifier for the repository.
-     * @param string $objectId The identifier for the object.
-     * @param string $folderId The folder into which the object is to be filed.
-     * @param boolean $allVersions Add all versions of the object to the folder if the repository supports
-     *      version-specific filing. Defaults to <code>true</code>.
+     * @param string                      $repositoryId The identifier for the repository.
+     * @param string                      $objectId The identifier for the object.
+     * @param string                      $folderId The folder into which the object is to be filed.
+     * @param boolean                     $allVersions Add all versions of the object to the folder if the repository
+     *     supports version-specific filing. Defaults to <code>true</code>.
      * @param ExtensionDataInterface|null $extension
      */
     public function addObjectToFolder(
@@ -35,15 +35,24 @@ class MultiFilingService extends AbstractBrowserBindingService implements MultiF
         $allVersions = true,
         ExtensionDataInterface $extension = null
     ) {
-        // TODO: Implement addObjectToFolder() method.
+        $url = $this->getObjectUrl($repositoryId, $objectId);
+
+        $queryArray = array(
+            Constants::CONTROL_CMISACTION => Constants::CMISACTION_ADD_OBJECT_TO_FOLDER,
+            Constants::PARAM_SUCCINCT => $this->getSuccinct() ? 'true' : 'false',
+            Constants::PARAM_FOLDER_ID => $folderId,
+            Constants::PARAM_ALL_VERSIONS => $allVersions ? 'true' : 'false',
+        );
+
+        $this->post($url, $queryArray)->json();
     }
 
     /**
      * Removes an existing fileable non-folder object from a folder.
      *
-     * @param string $repositoryId The identifier for the repository.
-     * @param string $objectId The identifier for the object.
-     * @param string|null $folderId The folder from which the object is to be removed.
+     * @param string                      $repositoryId The identifier for the repository.
+     * @param string                      $objectId The identifier for the object.
+     * @param string|null                 $folderId The folder from which the object is to be removed.
      *      If no value is specified, then the repository MUST remove the object from all folders in which it is
      *      currently filed.
      * @param ExtensionDataInterface|null $extension
