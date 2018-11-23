7c50dd49cf5bb0fddcda7fc625eb6e99ef3642b6
André Wuttig <andre.wuttig@scanacs.de>
2018-11-23 14:46:08 +0100

    [BUGFIX] fixes missing brackets in convertObjectInFolderList method which leads to Exception when index is not present

--- src/Converter/JsonConverter.php
+++ src/Converter/JsonConverter.php
@@ -1555,11 +1555,7 @@ class JsonConverter extends AbstractDataConverter
         $objectInFolderList = new ObjectInFolderList();
         $objectInFolderList->setObjects($objects);
         $objectInFolderList->setHasMoreItems((boolean) ($data[JSONConstants::JSON_OBJECTINFOLDERLIST_HAS_MORE_ITEMS] ?? false));
-        if (isset($data[JSONConstants::JSON_OBJECTINFOLDERLIST_NUM_ITEMS])) {
-            $objectInFolderList->setNumItems((integer) $data[JSONConstants::JSON_OBJECTINFOLDERLIST_NUM_ITEMS]);
-        } else {
-            $objectInFolderList->setNumItems(count($objects));
-        }
+        $objectInFolderList->setNumItems((Integer)($data[JSONConstants::JSON_OBJECTINFOLDERLIST_NUM_ITEMS] ?? count($objects)));
         $objectInFolderList->setExtensions($this->convertExtension($data, JSONConstants::getObjectInFolderListKeys()));
 
         return $objectInFolderList;
